4f4522bc42ac12b4ef8f9f4874c4b615188eab04
gongjun <1244081268@qq.com>
2022-07-04 23:52:00 +0800

    3

--- src/main/java/com/gj/config/MpConfig.java
+++ src/main/java/com/gj/config/MpConfig.java
@@ -3,5 +3,7 @@ package com.gj.config;
 public class MpConfig {
     public static void main(String[] args) {
         System.out.println(123);
+        System.out.println(123);
+        System.out.println(123);
     }
 }
